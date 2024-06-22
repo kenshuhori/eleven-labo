d5caa9482cbb86918f8d5627dd212f1eb3bd2ddb
Kenshu Hori <kenshuhori@gmail.com>
2024-06-22 13:51:51 +0900

    feat: yarn add -D @chakra-ui/storybook-addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@chakra-ui/storybook-addon",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
