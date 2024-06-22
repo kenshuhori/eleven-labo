a9c9d28343d6cd6bbc8552ef121a490754e9f137
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-06-22 13:36:29 +0900

    storybook の component を利用してみる (#12)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { Button } from "@/stories/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1 className={styles.main}>ベストイレブン研究所 | Eleven Labo</h1>;
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>ベストイレブン研究所 | Eleven Labo</h1>
+        <Button label={"クリック"} />
+      </main>
+    </>
+  );
 }
