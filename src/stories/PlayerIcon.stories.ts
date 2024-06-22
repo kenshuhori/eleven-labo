a40996b5136dc87869ef2a382ba3e3b38fd72b08
Kenshu Hori <kenshuhori@gmail.com>
2024-06-22 17:30:26 +0900

    feat: player-icon

--- src/stories/PlayerIcon.stories.ts
+++ src/stories/PlayerIcon.stories.ts
@@ -11,14 +11,27 @@ const meta = {
   argTypes: {
     backgroundColor: { control: "color" },
   },
-  args: { label: "PlayerIcon" },
+  args: { number: "PlayerIcon" },
 } satisfies Meta<typeof PlayerIcon>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
+export const Liverpool: Story = {
   args: {
-    label: "Base",
+    backgroundColor: "#C8102E",
+    borderColor: "#00B2A9",
+    color: "#F6EB61",
+    number: "100",
+  },
+};
+
+export const ManchesterCity: Story = {
+  args: {
+    backgroundColor: "#6CABDD",
+    borderColor: "#FFFFFF",
+    color: "#1C2C5B",
+    number: "100",
+    textStrokeColor: "#FFFFFF",
   },
 };
--- src/stories/PlayerIcon.tsx
+++ src/stories/PlayerIcon.tsx
@@ -14,42 +14,52 @@ import type { CSSProperties } from "react";
 interface PlayerIconProps {
   backgroundColor?: string;
   borderColor?: string;
-  label: string;
+  color?: string;
+  number: string;
+  textStrokeColor?: string;
 }
 
 export const PlayerIcon = ({
-  backgroundColor,
-  borderColor,
-  label,
+  backgroundColor = "#FFFFFF",
+  borderColor = "#000000",
+  color = "#000000",
+  number,
+  textStrokeColor,
   ...props
 }: PlayerIconProps) => {
   const style: CSSProperties = {
-    backgroundColor: backgroundColor ?? "red",
-    borderColor: borderColor ?? "yellow",
-    color: "white",
+    backgroundColor: backgroundColor,
+    border: `2px solid ${borderColor}`,
+    borderRadius: "50%",
+    color: color,
+    fontSize: "1.0rem",
+    fontWeight: "1000",
+    height: "40px",
+    WebkitTextStroke: textStrokeColor ? `1px ${"#ffffff"}` : "none",
+    width: "40px",
   };
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Button onClick={onOpen} style={style} type="button" {...props}>
-        {label}
+        {number}
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>選手選択</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <p>hoge</p>
+            <p>選手選択</p>
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+              閉じる
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
+            <Button variant="ghost">確定</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
