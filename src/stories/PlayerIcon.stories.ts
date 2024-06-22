d20e7e47f087c60cc83462fab18a6ec747ffcdbe
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-06-22 20:08:20 +0900

    player-icon をざっくり storybook で実装してみる (#15)
    * feat: player-icon
    
    * feat: add default
    
    * feat: yarn add react-select
    
    * feat: react-select を利用した セレクト
    
    * feat: モーダル簡素化

--- src/stories/PlayerIcon.stories.ts
+++ src/stories/PlayerIcon.stories.ts
@@ -11,14 +11,33 @@ const meta = {
   argTypes: {
     backgroundColor: { control: "color" },
   },
-  args: { label: "PlayerIcon" },
+  args: { number: "PlayerIcon" },
 } satisfies Meta<typeof PlayerIcon>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
+export const Default: Story = {
   args: {
-    label: "Base",
+    number: "??",
+  },
+};
+
+export const Liverpool: Story = {
+  args: {
+    backgroundColor: "#C8102E",
+    borderColor: "#00B2A9",
+    color: "#F6EB61",
+    number: "100",
+  },
+};
+
+export const ManchesterCity: Story = {
+  args: {
+    backgroundColor: "#6CABDD",
+    borderColor: "#FFFFFF",
+    color: "#1C2C5B",
+    number: "100",
+    textStrokeColor: "#FFFFFF",
   },
 };
--- src/stories/PlayerIcon.tsx
+++ src/stories/PlayerIcon.tsx
@@ -10,46 +10,62 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import type { CSSProperties } from "react";
+import Select from "react-select";
 
 interface PlayerIconProps {
   backgroundColor?: string;
   borderColor?: string;
-  label: string;
+  color?: string;
+  number: string;
+  textStrokeColor?: string;
 }
 
 export const PlayerIcon = ({
   backgroundColor,
   borderColor,
-  label,
+  color,
+  number,
+  textStrokeColor,
   ...props
 }: PlayerIconProps) => {
   const style: CSSProperties = {
-    backgroundColor: backgroundColor ?? "red",
-    borderColor: borderColor ?? "yellow",
-    color: "white",
+    backgroundColor: backgroundColor ?? "#FFFFFF",
+    border: `2px solid ${borderColor ?? "#000000"}`,
+    borderRadius: "50%",
+    color: color ?? "#000000",
+    fontSize: "1.0rem",
+    fontWeight: "1000",
+    height: "40px",
+    WebkitTextStroke: textStrokeColor ? `1px ${"#ffffff"}` : "none",
+    width: "40px",
   };
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const options = [
+    { value: "player001", label: "M.Salah" },
+    { value: "player002", label: "A.Becker" },
+    { value: "player003", label: "V.Van Dijk" },
+    { value: "player004", label: "Mac Allister" },
+  ];
 
   return (
     <>
       <Button onClick={onOpen} style={style} type="button" {...props}>
-        {label}
+        {number}
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>選手選択</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <p>hoge</p>
+            <Select options={options} placeholder="選手を選択してください" />
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme="blue" onClick={onClose}>
+              閉じる
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
