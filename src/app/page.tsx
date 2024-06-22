41458b707a3b3fa195d8bc6c21a0ccd7847b4328
Kenshu Hori <kenshuhori@gmail.com>
2024-06-22 11:49:44 +0900

    yarn format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <h1 className={styles.main}>ベストイレブン研究所 | Eleven Labo</h1>
-  );
+  return <h1 className={styles.main}>ベストイレブン研究所 | Eleven Labo</h1>;
 }
