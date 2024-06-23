d22f597fb00c8d0d0a5bda43061b62361c8ed0e8
Kenshu Hori <kenshuhori@gmail.com>
2024-06-23 22:22:18 +0900

    feat: forwardRef を適用して 11 人並べる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,75 @@
 
 import { Header } from "@/stories/Header";
 import { PlayerIcon } from "@/stories/PlayerIcon";
-import { VStack } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const playerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (!playerRef.current) return;
+  const player002Ref = useRef<HTMLDivElement>(null);
+  const player003Ref = useRef<HTMLDivElement>(null);
+  const player004Ref = useRef<HTMLDivElement>(null);
+  const player005Ref = useRef<HTMLDivElement>(null);
+  const player006Ref = useRef<HTMLDivElement>(null);
+  const player007Ref = useRef<HTMLDivElement>(null);
+  const player008Ref = useRef<HTMLDivElement>(null);
+  const player009Ref = useRef<HTMLDivElement>(null);
+  const player010Ref = useRef<HTMLDivElement>(null);
+  const player011Ref = useRef<HTMLDivElement>(null);
 
-    const ballSvg = playerRef.current;
-    ballSvg.onpointermove = (event) => {
-      if (event.buttons) {
-        const horizon = ballSvg.offsetLeft + event.movementX;
-        if (horizon < 50 || horizon > window.innerWidth - 100) return;
-        ballSvg.style.left = `${ballSvg.offsetLeft + event.movementX}px`;
-        ballSvg.style.top = `${ballSvg.offsetTop + event.movementY}px`;
-        ballSvg.style.position = "absolute";
-        ballSvg.draggable = false;
-        ballSvg.setPointerCapture(event.pointerId);
-      }
-    };
-  }, []);
+  [
+    player002Ref,
+    player003Ref,
+    player004Ref,
+    player005Ref,
+    player006Ref,
+    player007Ref,
+    player008Ref,
+    player009Ref,
+    player010Ref,
+    player011Ref,
+  ].forEach((playerRef, index) => {
+    useEffect(() => {
+      if (!playerRef.current) return;
+
+      const ballSvg = playerRef.current;
+      ballSvg.onpointermove = (event) => {
+        if (event.buttons) {
+          const horizon = ballSvg.offsetLeft + event.movementX;
+          if (horizon < 50 || horizon > window.innerWidth - 100) return;
+          ballSvg.style.left = `${ballSvg.offsetLeft + event.movementX}px`;
+          ballSvg.style.top = `${ballSvg.offsetTop + event.movementY}px`;
+          ballSvg.style.position = "absolute";
+          ballSvg.draggable = false;
+          ballSvg.setPointerCapture(event.pointerId);
+        }
+      };
+    }, [playerRef]);
+  });
   return (
     <>
       <Header />
       <main>
-        <VStack>
-          <div className={styles.footballField}>
-            <div ref={playerRef}>
-              <PlayerIcon />
-            </div>
+        <div className={styles.footballField}>
+          <div>
+            <PlayerIcon className={styles.lw} ref={player011Ref} />
+            <PlayerIcon className={styles.cf} ref={player009Ref} />
+            <PlayerIcon className={styles.rw} ref={player007Ref} />
           </div>
-        </VStack>
+          <div>
+            <PlayerIcon className={styles.lih} ref={player010Ref} />
+            <PlayerIcon className={styles.cmf} ref={player004Ref} />
+            <PlayerIcon className={styles.rih} ref={player008Ref} />
+          </div>
+          <div>
+            <PlayerIcon className={styles.lb} ref={player003Ref} />
+            <PlayerIcon className={styles.lcb} ref={player006Ref} />
+            <PlayerIcon className={styles.rcb} ref={player005Ref} />
+            <PlayerIcon className={styles.rb} ref={player002Ref} />
+          </div>
+          <div>
+            <PlayerIcon className={styles.gk} />
+          </div>
+        </div>
       </main>
     </>
   );
--- src/stories/PlayerIcon.tsx
+++ src/stories/PlayerIcon.tsx
@@ -8,165 +8,178 @@ import {
   VStack,
   useDisclosure,
 } from "@chakra-ui/react";
-import { type CSSProperties, useCallback, useState } from "react";
+import {
+  type CSSProperties,
+  type ForwardedRef,
+  forwardRef,
+  useCallback,
+  useState,
+} from "react";
 import Select from "react-select";
 
 interface PlayerIconProps {
   backgroundColor?: string;
   borderColor?: string;
+  className?: string;
   color?: string;
   number?: number;
   textShadowColor?: string;
 }
 
-export const PlayerIcon = ({
-  backgroundColor,
-  borderColor,
-  color,
-  number,
-  textShadowColor,
-  ...props
-}: PlayerIconProps) => {
-  const defaultPlayer: Player = {
-    name: "???",
-    number: number ?? 99,
-    team: {
-      name: "Default",
-      backgroundColor: backgroundColor ?? "#FFFFFF",
-      borderColor: borderColor ?? "#000000",
-      color: color ?? "#000000",
-      textShadowColor: textShadowColor ?? null,
-    },
-  };
-
-  const style = (team: Player["team"]): CSSProperties => {
-    return {
-      backgroundColor: team.backgroundColor,
-      border: `2px solid ${team.borderColor}`,
-      borderRadius: "50%",
-      color: team.color,
-      fontSize: "1.4rem",
-      fontWeight: "1000",
-      height: "3.5rem",
-      textShadow: team.textShadowColor
-        ? `1px 1px ${team.textShadowColor}`
-        : "unset",
-      filter: "drop-shadow(2px 4px 2px #000000)",
-      width: "3.5rem",
+export const PlayerIcon = forwardRef(
+  (
+    {
+      backgroundColor,
+      borderColor,
+      color,
+      className,
+      number,
+      textShadowColor,
+      ...props
+    }: PlayerIconProps,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    const defaultPlayer: Player = {
+      name: "???",
+      number: number ?? 99,
+      team: {
+        name: "Default",
+        backgroundColor: backgroundColor ?? "#FFFFFF",
+        borderColor: borderColor ?? "#000000",
+        color: color ?? "#000000",
+        textShadowColor: textShadowColor ?? null,
+      },
     };
-  };
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  type Option = {
-    value: string;
-    label: string;
-  } & Player;
-  const [player, setPlayer] = useState<Player>(defaultPlayer);
-  const onChange = useCallback((selected: Option | null) => {
-    setPlayer({
-      name: selected?.name ?? "???",
-      number: selected?.number ?? 0,
-      team: {
-        name: selected?.team.name ?? "???",
-        backgroundColor: selected?.team.backgroundColor ?? "#FFFFFF",
-        borderColor: selected?.team.borderColor ?? "#000000",
-        color: selected?.team.color ?? "#000000",
-        textShadowColor: selected?.team.textShadowColor ?? null,
-      },
-    });
-  }, []);
-  const options: Option[] = [
-    {
-      value: "player001",
-      label: "M.Salah",
-      name: "M.Salah",
-      number: 11,
-      team: {
-        name: "Liverpool",
-        backgroundColor: "#C8102E",
-        borderColor: "#00B2A9",
-        color: "#F6EB61",
-        textShadowColor: null,
-      },
-    },
-    {
-      value: "player002",
-      label: "K.De Bruyne",
-      name: "K.De Bruyne",
-      number: 17,
-      team: {
-        name: "Manchester City",
-        backgroundColor: "#6CABDD",
-        borderColor: "#FFFFFF",
-        color: "#1C2C5B",
-        textShadowColor: "#FFFFFF",
-      },
-    },
-    {
-      value: "player003",
-      label: "M.Odegaard",
-      name: "M.Odegaard",
-      number: 8,
-      team: {
-        name: "Arsenal",
-        backgroundColor: "#EF0107",
-        borderColor: "#063672",
-        color: "#FFFFFF",
-        textShadowColor: "#9C824A",
-      },
-    },
-    {
-      value: "player004",
-      label: "H.Son",
-      name: "H.Son",
-      number: 7,
-      team: {
-        name: "Totenham",
-        color: "#132257",
-        backgroundColor: "#FFFFFF",
-        borderColor: "#132257",
-        textShadowColor: null,
-      },
-    },
-  ];
+    const style = (team: Player["team"]): CSSProperties => {
+      return {
+        backgroundColor: team.backgroundColor,
+        border: `2px solid ${team.borderColor}`,
+        borderRadius: "50%",
+        color: team.color,
+        fontSize: "1.4rem",
+        fontWeight: "1000",
+        height: "3.5rem",
+        textShadow: team.textShadowColor
+          ? `1px 1px ${team.textShadowColor}`
+          : "unset",
+        filter: "drop-shadow(2px 4px 2px #000000)",
+        width: "3.5rem",
+      };
+    };
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <>
-      <VStack>
-        <Button
-          onClick={onOpen}
-          style={style(player.team)}
-          type="button"
-          {...props}
-        >
-          {player.number}
-        </Button>
-        <Text style={playerNameStyle}>{player.name}</Text>
-      </VStack>
+    type Option = {
+      value: string;
+      label: string;
+    } & Player;
+    const [player, setPlayer] = useState<Player>(defaultPlayer);
+    const onChange = useCallback((selected: Option | null) => {
+      setPlayer({
+        name: selected?.name ?? "???",
+        number: selected?.number ?? 0,
+        team: {
+          name: selected?.team.name ?? "???",
+          backgroundColor: selected?.team.backgroundColor ?? "#FFFFFF",
+          borderColor: selected?.team.borderColor ?? "#000000",
+          color: selected?.team.color ?? "#000000",
+          textShadowColor: selected?.team.textShadowColor ?? null,
+        },
+      });
+    }, []);
+    const options: Option[] = [
+      {
+        value: "player001",
+        label: "M.Salah",
+        name: "M.Salah",
+        number: 11,
+        team: {
+          name: "Liverpool",
+          backgroundColor: "#C8102E",
+          borderColor: "#00B2A9",
+          color: "#F6EB61",
+          textShadowColor: null,
+        },
+      },
+      {
+        value: "player002",
+        label: "K.De Bruyne",
+        name: "K.De Bruyne",
+        number: 17,
+        team: {
+          name: "Manchester City",
+          backgroundColor: "#6CABDD",
+          borderColor: "#FFFFFF",
+          color: "#1C2C5B",
+          textShadowColor: "#FFFFFF",
+        },
+      },
+      {
+        value: "player003",
+        label: "M.Odegaard",
+        name: "M.Odegaard",
+        number: 8,
+        team: {
+          name: "Arsenal",
+          backgroundColor: "#EF0107",
+          borderColor: "#063672",
+          color: "#FFFFFF",
+          textShadowColor: "#9C824A",
+        },
+      },
+      {
+        value: "player004",
+        label: "H.Son",
+        name: "H.Son",
+        number: 7,
+        team: {
+          name: "Totenham",
+          color: "#132257",
+          backgroundColor: "#FFFFFF",
+          borderColor: "#132257",
+          textShadowColor: null,
+        },
+      },
+    ];
 
-      <Modal isOpen={isOpen} onClose={onClose} size="md">
-        <ModalOverlay />
-        <ModalContent style={{ top: "15vh" }}>
-          {/* <ModalHeader>選手選択</ModalHeader>
+    return (
+      <div className={className} ref={ref}>
+        <VStack>
+          <Button
+            onClick={onOpen}
+            style={style(player.team)}
+            type="button"
+            {...props}
+          >
+            {player.number}
+          </Button>
+          <Text style={playerNameStyle}>{player.name}</Text>
+        </VStack>
+
+        <Modal isOpen={isOpen} onClose={onClose} size="md">
+          <ModalOverlay />
+          <ModalContent style={{ top: "15vh" }}>
+            {/* <ModalHeader>選手選択</ModalHeader>
           <ModalCloseButton /> */}
-          <ModalBody>
-            <Select
-              onChange={onChange}
-              options={options}
-              placeholder="選手を選択"
-            />
-          </ModalBody>
+            <ModalBody>
+              <Select
+                onChange={onChange}
+                options={options}
+                placeholder="選手を選択"
+              />
+            </ModalBody>
 
-          {/* <ModalFooter>
+            {/* <ModalFooter>
             <Button colorScheme="blue" onClick={onClose}>
               閉じる
             </Button>
           </ModalFooter> */}
-        </ModalContent>
-      </Modal>
-    </>
-  );
-};
+          </ModalContent>
+        </Modal>
+      </div>
+    );
+  },
+);
 
 const playerNameStyle: CSSProperties = {
   fontSize: "1.0rem",
