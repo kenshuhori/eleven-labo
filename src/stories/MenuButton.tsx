fd71f6e0086faffaf9efdd2195cf49aadf798302
Kenshu Hori <kenshuhori@gmail.com>
2024-06-23 10:15:49 +0900

    feat: icon調整

--- src/stories/MenuButton.tsx
+++ src/stories/MenuButton.tsx
@@ -1,13 +1,16 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
 import {
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  useDisclosure,
+  MenuButton as ChakraMenuButton,
+  Icon,
+  IconButton,
+  Menu,
+  MenuItem,
+  MenuList,
 } from "@chakra-ui/react";
+import Link from "next/link";
+import { FaBook } from "react-icons/fa6";
+import { GiSoccerBall } from "react-icons/gi";
+import { RiTeamFill } from "react-icons/ri";
 
 interface MenuProps {
   height?: string;
@@ -15,50 +18,31 @@ interface MenuProps {
 }
 
 export const MenuButton = ({ height = "25", width = "24" }: MenuProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
-    <div>
-      <Button onClick={onOpen} bg={"#FFFFFF"}>
-        <svg
-          width={width}
-          height={height}
-          viewBox="0 0 25 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>menu</title>
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M3.1001 6.00001C3.1001 5.50295 3.50304 5.10001 4.0001 5.10001H20.0001C20.4972 5.10001 20.9001 5.50295 20.9001 6.00001C20.9001 6.49706 20.4972 6.90001 20.0001 6.90001H4.0001C3.50304 6.90001 3.1001 6.49706 3.1001 6.00001Z"
-            fill="black"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M3.09961 12C3.09961 11.5029 3.41653 11.1 3.80747 11.1H16.3917C16.7827 11.1 17.0996 11.5029 17.0996 12C17.0996 12.4971 16.7827 12.9 16.3917 12.9H3.80747C3.41653 12.9 3.09961 12.4971 3.09961 12Z"
-            fill="black"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M3.1001 18C3.1001 17.5029 3.50304 17.1 4.0001 17.1H20.0001C20.4972 17.1 20.9001 17.5029 20.9001 18C20.9001 18.4971 20.4972 18.9 20.0001 18.9H4.0001C3.50304 18.9 3.1001 18.4971 3.1001 18Z"
-            fill="black"
-          />
-        </svg>
-      </Button>
-
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader>{/* なにもなし */}</DrawerHeader>
-
-          <DrawerBody>{/* ここにメニューを書く */}</DrawerBody>
-
-          <DrawerFooter>{/* なにもなし */}</DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </div>
+    <Menu>
+      <ChakraMenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<HamburgerIcon />}
+        variant="outline"
+      />
+      <MenuList>
+        <Link href="/themes" passHref>
+          <MenuItem icon={<Icon as={FaBook} boxSize={5} />}>
+            テーマ ~Themes~
+          </MenuItem>
+        </Link>
+        <Link href="/teams" passHref>
+          <MenuItem icon={<Icon as={GiSoccerBall} boxSize={5} />}>
+            チーム ~Teams~
+          </MenuItem>
+        </Link>
+        <Link href="/aboutUs" passHref>
+          <MenuItem icon={<Icon as={RiTeamFill} boxSize={5} />}>
+            About Us
+          </MenuItem>
+        </Link>
+      </MenuList>
+    </Menu>
   );
 };
