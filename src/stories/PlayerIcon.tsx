be33f418adbbfddd4eab18d73a5cbcc9002c084d
Kenshu Hori <kenshuhori@gmail.com>
2024-06-23 11:34:54 +0900

    feat: 選択した選手のチームにアイコンを変化

--- src/stories/PlayerIcon.tsx
+++ src/stories/PlayerIcon.tsx
@@ -30,16 +30,44 @@ export const PlayerIcon = ({
   textStrokeColor,
   ...props
 }: PlayerIconProps) => {
-  const style: CSSProperties = {
-    backgroundColor: backgroundColor ?? "#FFFFFF",
-    border: `2px solid ${borderColor ?? "#000000"}`,
-    borderRadius: "50%",
-    color: color ?? "#000000",
-    fontSize: "1.0rem",
-    fontWeight: "1000",
-    height: "40px",
-    WebkitTextStroke: textStrokeColor ? `1px ${"#ffffff"}` : "none",
-    width: "40px",
+  type Player = {
+    name: string;
+    number: number;
+    team: {
+      name: string;
+      backgroundColor: string;
+      borderColor: string;
+      color: string;
+      textStrokeColor: string | null;
+    };
+  };
+
+  const defaultPlayer: Player = {
+    name: "???",
+    number: 99,
+    team: {
+      name: "Default",
+      backgroundColor: "#FFFFFF",
+      borderColor: "#000000",
+      color: "#000000",
+      textStrokeColor: null,
+    },
+  };
+
+  const style = (team: Player["team"]): CSSProperties => {
+    return {
+      backgroundColor: team.backgroundColor,
+      border: `2px solid ${team.borderColor}`,
+      borderRadius: "50%",
+      color: team.color,
+      fontSize: "1.0rem",
+      fontWeight: "1000",
+      height: "40px",
+      WebkitTextStroke: team.textStrokeColor
+        ? `1px ${team.textStrokeColor}`
+        : "none",
+      width: "40px",
+    };
   };
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -48,34 +76,92 @@ export const PlayerIcon = ({
     label: string;
     name: string;
     number: number;
+    team: {
+      name: string;
+      backgroundColor: string;
+      borderColor: string;
+      color: string;
+      textStrokeColor: string | null;
+    };
   };
-  type Player = {
-    name: string;
-    number: number;
-  };
-  const [player, setPlayer] = useState<Player>({ name: "???", number: 99 });
+  const [player, setPlayer] = useState<Player>(defaultPlayer);
   const onChange = useCallback((selected: Option | null) => {
     setPlayer({
       name: selected?.name ?? "???",
       number: selected?.number ?? 0,
+      team: {
+        name: selected?.team.name ?? "???",
+        backgroundColor: selected?.team.backgroundColor ?? "#FFFFFF",
+        borderColor: selected?.team.borderColor ?? "#000000",
+        color: selected?.team.color ?? "#000000",
+        textStrokeColor: selected?.team.textStrokeColor ?? null,
+      },
     });
   }, []);
   const options: Option[] = [
-    { value: "player001", label: "M.Salah", name: "M.Salah", number: 11 },
-    { value: "player002", label: "A.Becker", name: "A.Becker", number: 1 },
-    { value: "player003", label: "V.Van Dijk", name: "V.Van Dijk", number: 4 },
+    {
+      value: "player001",
+      label: "M.Salah",
+      name: "M.Salah",
+      number: 11,
+      team: {
+        name: "Liverpool",
+        backgroundColor: "#C8102E",
+        borderColor: "#00B2A9",
+        color: "#F6EB61",
+        textStrokeColor: null,
+      },
+    },
+    {
+      value: "player002",
+      label: "K.De Bruyne",
+      name: "K.De Bruyne",
+      number: 17,
+      team: {
+        name: "Manchester City",
+        backgroundColor: "#6CABDD",
+        borderColor: "#FFFFFF",
+        color: "#1C2C5B",
+        textStrokeColor: "#FFFFFF",
+      },
+    },
+    {
+      value: "player003",
+      label: "V.Van Dijk",
+      name: "V.Van Dijk",
+      number: 4,
+      team: {
+        name: "Liverpool",
+        backgroundColor: "#C8102E",
+        borderColor: "#00B2A9",
+        color: "#F6EB61",
+        textStrokeColor: null,
+      },
+    },
     {
       value: "player004",
       label: "Mac Allister",
       name: "Mac Allister",
       number: 10,
+      team: {
+        name: "Liverpool",
+        color: "#FF0000",
+        backgroundColor: "#FFFFFF",
+        borderColor: "#00B2A9",
+        textStrokeColor: null,
+      },
     },
   ];
 
   return (
     <>
       <VStack>
-        <Button onClick={onOpen} style={style} type="button" {...props}>
+        <Button
+          onClick={onOpen}
+          style={style(player.team)}
+          type="button"
+          {...props}
+        >
           {player.number}
         </Button>
         <Text>{player.name}</Text>
