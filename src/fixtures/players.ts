2d3853f554cc0bc697c35317b8d070a40db9f36a
Kenshu Hori <kenshuhori@gmail.com>
2024-06-23 23:54:14 +0900

    feat: 選手選択してもアイコンがずれないようにcenterへ

---
+++ src/fixtures/players.ts
@@ -0,0 +1,123 @@
+export const players: Player[] = [
+  {
+    name: "M.Salah",
+    number: 11,
+    team: {
+      name: "Liverpool",
+      backgroundColor: "#C8102E",
+      borderColor: "#00B2A9",
+      color: "#F6EB61",
+      textShadowColor: null,
+    },
+  },
+  {
+    name: "K.De Bruyne",
+    number: 17,
+    team: {
+      name: "Manchester City",
+      backgroundColor: "#6CABDD",
+      borderColor: "#FFFFFF",
+      color: "#1C2C5B",
+      textShadowColor: "#FFFFFF",
+    },
+  },
+  {
+    name: "M.Odegaard",
+    number: 8,
+    team: {
+      name: "Arsenal",
+      backgroundColor: "#EF0107",
+      borderColor: "#063672",
+      color: "#FFFFFF",
+      textShadowColor: "#9C824A",
+    },
+  },
+  {
+    name: "H.Son",
+    number: 7,
+    team: {
+      name: "Totenham",
+      color: "#132257",
+      backgroundColor: "#FFFFFF",
+      borderColor: "#132257",
+      textShadowColor: null,
+    },
+  },
+  {
+    name: "T.Silva",
+    number: 6,
+    team: {
+      name: "Chelsea",
+      color: "#FFFFFF",
+      backgroundColor: "#034694",
+      borderColor: "#DBA111",
+      textShadowColor: null,
+    },
+  },
+  {
+    name: "B.Fernandes",
+    number: 18,
+    team: {
+      name: "Manchester United",
+      color: "#000000",
+      backgroundColor: "#DA291C",
+      borderColor: "#000000",
+      textShadowColor: "#FBE122",
+    },
+  },
+  {
+    name: "K.Mitoma",
+    number: 22,
+    team: {
+      name: "Brighton",
+      color: "#FFCD00",
+      backgroundColor: "#0057B8",
+      borderColor: "#FFFFFF",
+      textShadowColor: null,
+    },
+  },
+  {
+    name: "M.Antonio",
+    number: 9,
+    team: {
+      name: "West Ham",
+      color: "#F3D459",
+      backgroundColor: "#7A263A",
+      borderColor: "#1BB1E7",
+      textShadowColor: null,
+    },
+  },
+  {
+    name: "J.Sarr",
+    number: 1,
+    team: {
+      name: "Wolverhampton",
+      color: "#231F20",
+      backgroundColor: "#FDB913",
+      borderColor: "#231F20",
+      textShadowColor: "#FFFFFF",
+    },
+  },
+  {
+    name: "B.Guimaraes",
+    number: 39,
+    team: {
+      name: "Newcastle",
+      color: "#F1BE48",
+      backgroundColor: "#241F20",
+      borderColor: "#41B6E6",
+      textShadowColor: "#FFFFFF",
+    },
+  },
+  {
+    name: "L.Digne",
+    number: 12,
+    team: {
+      name: "Aston Villa",
+      color: "#670E36",
+      backgroundColor: "#95BFE5",
+      borderColor: "#FEE505",
+      textShadowColor: null,
+    },
+  },
+];
