f7be5c15925c4ec1dc4fc4d25219e8bed7de29c7
Kenshu Hori <kenshuhori@gmail.com>
2024-06-23 21:15:06 +0900

    feat: styleを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Header } from "@/stories/Header";
 import { PlayerIcon } from "@/stories/PlayerIcon";
+import { VStack } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
@@ -13,6 +14,8 @@ export default function Home() {
     const ballSvg = playerRef.current;
     ballSvg.onpointermove = (event) => {
       if (event.buttons) {
+        const horizon = ballSvg.offsetLeft + event.movementX;
+        if (horizon < 50 || horizon > window.innerWidth - 100) return;
         ballSvg.style.left = `${ballSvg.offsetLeft + event.movementX}px`;
         ballSvg.style.top = `${ballSvg.offsetTop + event.movementY}px`;
         ballSvg.style.position = "absolute";
@@ -24,10 +27,14 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className={styles.main}>
-        <div ref={playerRef}>
-          <PlayerIcon />
-        </div>
+      <main>
+        <VStack>
+          <div className={styles.footballField}>
+            <div ref={playerRef}>
+              <PlayerIcon />
+            </div>
+          </div>
+        </VStack>
       </main>
     </>
   );
