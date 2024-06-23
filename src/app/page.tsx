026567c7b6a75daba861164431141192fd073469
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-06-23 16:58:00 +0900

    PlayerIcon を動かす (#21)
    * feat: ballのsvgを動かす
    
    * feat: playerIcon を動かす

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import { Header } from "@/stories/Header";
+import { PlayerIcon } from "@/stories/PlayerIcon";
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const playerRef = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    if (!playerRef.current) return;
+
+    const ballSvg = playerRef.current;
+    ballSvg.onpointermove = (event) => {
+      if (event.buttons) {
+        ballSvg.style.left = `${ballSvg.offsetLeft + event.movementX}px`;
+        ballSvg.style.top = `${ballSvg.offsetTop + event.movementY}px`;
+        ballSvg.style.position = "absolute";
+        ballSvg.draggable = false;
+        ballSvg.setPointerCapture(event.pointerId);
+      }
+    };
+  }, []);
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h1>ベストイレブン研究所 | Eleven Labo</h1>
+        <div ref={playerRef}>
+          <PlayerIcon />
+        </div>
       </main>
     </>
   );
