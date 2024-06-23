e1ffe9b2d1d3799bbebc70093c7e7308f7b0f2a3
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-06-23 19:17:18 +0900

    スマホでドラッグ、画像やヘッダの大きさを調整、viewport 設定してinputのfocusで拡大させない (#23)
    * feat: 大きさ修正
    
    * feat: viewport 設定
    
    * スマホでドラッグ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Providers } from "./providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "ベストイレブン研究所 | Eleven Labo",
   description: "君が考えるベストイレブンを全世界に発信しよう！",
+};
+
+export const viewport: Viewport = {
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  width: "device-width",
 };
 
 export default function RootLayout({
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>
+      <body style={{ overflow: "hidden" }}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/stories/PlayerIcon.tsx
+++ src/stories/PlayerIcon.tsx
@@ -47,12 +47,12 @@ export const PlayerIcon = ({
       color: team.color,
       fontSize: "1rem",
       fontWeight: "1000",
-      height: "3rem",
+      height: "3.5rem",
       textShadow: team.textShadowColor
         ? `1px 1px ${team.textShadowColor}`
         : "unset",
-      filter: "drop-shadow(2px 4px 6px #000000)",
-      width: "3rem",
+      filter: "drop-shadow(2px 4px 2px #000000)",
+      width: "3.5rem",
     };
   };
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -172,6 +172,5 @@ const playerNameStyle: CSSProperties = {
   fontSize: "1.0rem",
   fontWeight: "1000",
   color: "#FFFFFF",
-  textShadow: "1px 1px #000000",
-  filter: "drop-shadow(2px 4px 6px #000000)",
+  textShadow: "2px 2px #000000",
 };
