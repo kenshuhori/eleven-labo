a9912120d49c4bf260f35f38651983b4483260ef
Kenshu Hori <kenshuhori@gmail.com>
2024-07-03 23:44:03 -0700

    feat: Set environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+};
 
 export default nextConfig;
