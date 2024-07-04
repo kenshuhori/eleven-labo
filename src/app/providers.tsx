374df1a4d89a5098cfdf236e7da3f254be94056b
Kenshu Hori <kenshuhori@gmail.com>
2024-07-04 00:08:00 -0700

    feat: add clerk provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ClerkProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </ClerkProvider>
+  )
 }
