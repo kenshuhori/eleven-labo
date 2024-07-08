39defa25cb4b356f53ef2ff207a0ec0b64090ebd
Kenshu Hori <kenshuhori@gmail.com>
2024-07-08 16:14:29 -0600

    feat: componentsディレクトリを切ってコンポーネントを配置

--- src/stories/FootballField.stories.ts
+++ src/stories/FootballField.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { FootballField } from "./FootballField";
+import { FootballField } from "../components/FootballField";
 
 const meta = {
   title: "ElevenLabo/FootballField",
