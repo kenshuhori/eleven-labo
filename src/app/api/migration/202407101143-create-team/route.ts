c5864f23c4113bfb77663cb353cd4dc776fc62a6
Kenshu Hori <kenshuhori@gmail.com>
2024-07-10 11:53:12 -0600

    feat: migrate for create team

---
+++ src/app/api/migration/202407101143-create-team/route.ts
@@ -0,0 +1,12 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE Teams ( Name varchar(255), BackgroundColor varchar(255), BorderColor varchar(255), Color varchar(255), TextShadowColor varchar(255) );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
