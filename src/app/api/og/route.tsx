601b7537134f44aba3c0171410626bc840a2c100
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-07-09 22:38:46 -0600

    vercel/og を組み込み (#44)
    * feat: yarn add @vercel/og
    
    * feat: add api/og/route.tsx
    
    * feat: ogp画像を進捗させる
    
    * fix: build 修正
    
    * feat: ogp

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,75 @@
+import { EyeCatchOgp } from "@public/eyeCatchOgp";
+import { ServiceLogoOgp } from "@public/serviceLogoOgp";
+import { ImageResponse } from "next/og";
+import type { NextRequest } from "next/server";
+import type { CSSProperties } from "react";
+
+export const runtime = "edge";
+
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "23-24のプレミアリーグベスト11を考えよう！！";
+
+    return new ImageResponse(
+      <div style={baseStyle}>
+        <div style={mainStyle}>
+          <div style={titleStyle}>{title}</div>
+          <EyeCatchOgp />
+        </div>
+        <div style={serviceStyle}>
+          <ServiceLogoOgp />
+        </div>
+      </div>,
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (e) {
+    return new Response("Failed to generate the image", {
+      status: 500,
+    });
+  }
+}
+
+const baseStyle: CSSProperties = {
+  alignItems: "center",
+  background: "#289A0B",
+  display: "flex",
+  height: "100%",
+  justifyContent: "center",
+  width: "100%",
+};
+
+const mainStyle: CSSProperties = {
+  background: "#FFF6C5",
+  borderRadius: "20px",
+  boxShadow: "0 0 8px #777777",
+  display: "flex",
+  gap: "30px",
+  height: "570px",
+  justifyContent: "space-between",
+  padding: "80px 50px 80px 50px",
+  width: "1140px",
+};
+
+const titleStyle: CSSProperties = {
+  fontSize: "60px",
+  fontFamily: "Roboto",
+  fontWeight: "bold",
+};
+
+const serviceStyle: CSSProperties = {
+  bottom: "80px",
+  display: "flex",
+  left: "80px",
+  position: "absolute",
+};
+
+const logoStyle: CSSProperties = {
+  fontSize: "40px",
+};
