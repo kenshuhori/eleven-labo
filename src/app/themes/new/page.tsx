fd6ab8c41e709ab129688e0c6462a1f07aefd462
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2024-07-11 23:28:32 -0600

    themes テーブル作成 & データ作成 (#50)
    * feat: themes テーブル作成
    
    * feat: データ生成
    
    * feat: add revalidate and redirect

---
+++ src/app/themes/new/page.tsx
@@ -0,0 +1,34 @@
+import { createTheme } from "@/app/lib/actions";
+import type { CSSProperties } from "react";
+
+export default async function Page() {
+  return (
+    <>
+      <main>
+        <form action={createTheme} style={baseStyle}>
+          <label>
+            Title
+            <input type="text" name="title" />
+          </label>
+          <label>
+            likeCount
+            <input type="number" name="like_count" />
+          </label>
+          <label>
+            postCount
+            <input type="number" name="post_count" />
+          </label>
+          <button type="submit">Create</button>
+        </form>
+      </main>
+    </>
+  );
+}
+
+const baseStyle: CSSProperties = {
+  alignItems: "center",
+  backgroundColor: "#EEEEEE",
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px",
+};
