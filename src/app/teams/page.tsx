33835a71adec1a83189e0fe140020a46927df654
Kenshu Hori <kenshuhori@gmail.com>
2024-07-15 09:26:58 +0900

    feat: ページコンポーネント名をPageへ

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className={styles.main}>
       <h1>チーム一覧 | Eleven Labo</h1>
