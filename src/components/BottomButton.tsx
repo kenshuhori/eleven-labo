09e5797ce78069edc1434eb0248dc5c646141246
Kenshu Hori <kenshuhori@gmail.com>
2024-07-17 05:04:51 +0900

    feat: bottomButton のストーリー修正

--- src/components/BottomButton.tsx
+++ src/components/BottomButton.tsx
@@ -4,11 +4,12 @@ import React, { type CSSProperties } from "react";
 interface BottomButtonProps {
   label: string;
   href: string;
+  style?: CSSProperties;
 }
 
-export const BottomButton = ({ label, href }: BottomButtonProps) => {
+export const BottomButton = ({ label, href, style }: BottomButtonProps) => {
   return (
-    <div style={baseStyle}>
+    <div style={{ ...baseStyle, ...style }}>
       <Link href={href} style={buttonStyle}>
         {label}
       </Link>
