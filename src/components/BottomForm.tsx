02e5219f1168cffa1c3c5e4a7f4aa0bcd9907c8c
Kenshu Hori <kenshuhori@gmail.com>
2024-07-17 20:05:16 +0900

    feat: スタイル調整

--- src/components/BottomForm.tsx
+++ src/components/BottomForm.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import React, { type CSSProperties } from "react";
+import { Button, Textarea } from "@chakra-ui/react";
+import type { CSSProperties } from "react";
 
 interface BottomFormProps {
   label: string;
@@ -9,23 +9,23 @@ interface BottomFormProps {
 
 export const BottomForm = ({ label, href, style }: BottomFormProps) => {
   return (
-    <div style={{ ...baseStyle, ...style }}>
-      <Link href={href} style={buttonStyle}>
-        {label}
-      </Link>
-    </div>
+    <form style={{ ...baseStyle, ...style }}>
+      <textarea placeholder={"コメントを追加..."} style={textareaStyle} />
+      <Button style={buttonStyle}>{label}</Button>
+    </form>
   );
 };
 
 const baseStyle: CSSProperties = {
-  backgroundColor: "#FFFFFF",
+  alignItems: "center",
   position: "sticky",
   left: 0,
   right: 0,
   bottom: 0,
   display: "flex",
-  flexDirection: "column",
-  padding: "12px",
+  flexDirection: "row",
+  gap: "1rem",
+  padding: "1rem",
   boxShadow: "0 -2px 4px 0 #DDDDDD",
   width: "100%",
   zIndex: 100,
@@ -38,5 +38,13 @@ const buttonStyle: CSSProperties = {
   fontWeight: "700",
   lineHeight: "38px",
   textAlign: "center",
+  width: "5rem",
+};
+
+const textareaStyle: React.CSSProperties = {
+  fontSize: "1.1rem",
+  height: "4rem",
+  outline: "none",
+  resize: "none",
   width: "100%",
 };
