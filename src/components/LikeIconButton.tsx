0c02937983a9e76622ab1d39017c94e80fe9a58c
Kenshu Hori <kenshuhori@gmail.com>
2024-07-18 05:51:29 +0900

    feat: like カウントを上下させる

--- src/components/LikeIconButton.tsx
+++ src/components/LikeIconButton.tsx
@@ -1,6 +1,6 @@
 import { likedColorCode } from "@/constants";
 import { HeartIcon } from "@public/heartIcon";
-import React, { type CSSProperties } from "react";
+import React, { useCallback, useState, type CSSProperties } from "react";
 
 interface Props {
   count: number;
@@ -9,10 +9,24 @@ interface Props {
 }
 
 export const LikeIconButton = ({ count, liked, style }: Props) => {
+  const [like, setLike] = useState(liked);
+  const [likeCount, setLikeCount] = useState(count);
+
+  const onClick = useCallback(() => {
+    if (like) {
+      setLikeCount(likeCount - 1);
+    } else {
+      setLikeCount(likeCount + 1);
+    }
+    setLike(!like);
+  }, [like, likeCount]);
+
   return (
     <div style={{ ...baseStyle, ...style }}>
-      <HeartIcon style={liked ? likedIconStyle : iconStyle} />
-      <div>{count} likes</div>
+      <button onClick={onClick} type="button">
+        <HeartIcon style={like ? likedIconStyle : iconStyle} />
+      </button>
+      <div>{likeCount} likes</div>
     </div>
   );
 };
@@ -33,6 +47,7 @@ const likedIconStyle: CSSProperties = {
 
 const iconStyle: CSSProperties = {
   fill: "#FFFFFF",
+  filter: "drop-shadow(0 0 2px #000000)",
   height: "1.2rem",
   width: "1.2rem",
 };
