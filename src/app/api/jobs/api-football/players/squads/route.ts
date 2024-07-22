344e98d11b2cd83eefafe6f5e1ba990d16a89f63
Kenshu Hori <kenshuhori@gmail.com>
2024-07-22 22:06:49 +0900

    feat: team テーブルの id を not autoincrement へ

---
+++ src/app/api/jobs/api-football/players/squads/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    if (!checkPermission(request)) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // ここに処理を記述
+    const { searchParams } = new URL(request.url);
+    const team = searchParams.get("team") || 40;
+
+    const url = `https://v3.football.api-sports.io/players/squads?team=${team}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "f5caed64f266d47cb48b4f229f56d0c4",
+        "x-rapidapi-host": "v3.football.api-sports.io",
+      },
+      redirect: "follow",
+    });
+    const players = await response.json();
+
+    return NextResponse.json({ result: players }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+const checkPermission = (request: Request) => {
+  const authHeader = request.headers.get("authorization");
+  const { orgSlug } = auth();
+
+  // ※ authorization ヘッダーには “Bearer ” がプレフィクスにつくことに注意
+  if (authHeader === `Bearer ${process.env.CRON_SECRET}`) {
+    return true;
+  }
+
+  if (orgSlug === "administrator") {
+    return true;
+  }
+
+  return false;
+};
