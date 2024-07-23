b9dca024211955b20b94e6b81ca22ab726dd75bc
Kenshu Hori <kenshuhori@gmail.com>
2024-07-23 20:16:17 +0900

    feat: 新規投稿に渡すtheme をdb から取得

--- src/app/(service-page)/themes/[themeId]/posts/new/page.tsx
+++ src/app/(service-page)/themes/[themeId]/posts/new/page.tsx
@@ -1,18 +1,25 @@
+"use client";
+
+import { getTheme } from "@/app/actions";
 import { PostForm } from "@/components/PostForm";
-import { defaultTheme } from "@/fixtures/themes";
 import type { CSSProperties } from "react";
+import useSWR from "swr";
 
 interface PageProps {
   themeId: string;
 }
 
 export default function Page({ params }: { params: PageProps }) {
-  const themeId = params.themeId;
-  const theme = defaultTheme;
+  const { themeId } = params;
+  const { data: theme, error, isLoading } = useSWR(`/themes/${themeId}`, getTheme);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <main style={baseStyle}>
-      <PostForm theme={theme} />
+      {isLoading || theme === undefined ? <p>Loading...</p> : <PostForm theme={theme} />}
     </main>
   );
 }
