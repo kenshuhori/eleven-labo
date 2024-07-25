adf3ba84f0cff73a87b0ff0e040f10a44a145ed5
Kenshu Hori <kenshuhori@gmail.com>
2024-07-25 21:14:49 +0900

    feat: テーマに投稿を含めて返す

--- src/app/actions/theme/get.ts
+++ src/app/actions/theme/get.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import { prisma } from "@/prisma";
-import type { Theme } from "@prisma/client";
+import type { Post, Theme } from "@prisma/client";
+
+export type ThemeWithPostsResult = {
+  Posts: Post[];
+} & Theme;
 
 export const getTheme = async (key: string) => {
   const pathStr = key.split("/")[1];
@@ -22,4 +26,28 @@ export const getTheme = async (key: string) => {
       id,
     },
   }) as Promise<Theme>;
+};
+
+export const getThemeWithPosts = async (key: string) => {
+  const pathStr = key.split("/")[1];
+  const idStr = key.split("/")[2];
+
+  if (pathStr !== "themes" || idStr === undefined) {
+    throw new Error("Key not found");
+  }
+
+  const id = Number(idStr);
+
+  if (Number.isNaN(id)) {
+    throw new Error("Invalid key");
+  }
+
+  return prisma.theme.findUnique({
+    include: {
+      Posts: true,
+    },
+    where: {
+      id,
+    },
+  }) as Promise<ThemeWithPostsResult>;
 };
