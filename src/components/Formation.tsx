1f2709e70233f1fb19a579f2a8d124e30088e4d1
Kenshu Hori <kenshuhori@gmail.com>
2024-07-27 07:43:51 +0900

    feat: 選手選択の選手データ読み込みに無駄があるので最適化

--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -1,24 +1,18 @@
 import type React from "react";
-import { Fragment, createContext, useCallback, useEffect, useRef, useState } from "react";
+import { Fragment, useCallback, useRef, useState } from "react";
 import { FootballField } from "./FootballField";
 import { PlayerIcon } from "./PlayerIcon";
 import "@/styles/formations.css";
-import { listPlayer } from "@/app/actions";
 import { FormationIcon } from "@/components/FormationIcon";
 import { defaultPlayer } from "@/fixtures/players";
 import { positionCodes } from "@/fixtures/positionCodes";
-import { teams } from "@/fixtures/teams";
-import { groupedPlayers } from "@/utils/groupPlayer";
 import { Skeleton } from "@chakra-ui/react";
-import useSWR from "swr";
 
 interface FormationProps {
   formationCode: Formation["code"];
   readonly?: boolean;
   style?: React.CSSProperties;
 }
-
-export const PlayerSelectOptionsContext = createContext<GroupedPlayerSelectOption[]>([]);
 
 export const Formation = ({ formationCode, readonly = false, style }: FormationProps) => {
   const onChange = useCallback((selected: FormationSelectOption | null) => {
@@ -27,38 +21,28 @@ export const Formation = ({ formationCode, readonly = false, style }: FormationP
     setFormationClass(`formation-${selected?.code}`);
   }, []);
 
-  const [players, setPlayers] = useState<GroupedPlayerSelectOption[]>([]);
-  const { data, error, isLoading } = useSWR("/players", listPlayer);
-
-  useEffect(() => {
-    const groupedOptions: GroupedPlayerSelectOption[] = groupedPlayers(data, teams);
-    setPlayers(groupedOptions);
-  }, [data]);
-
   const [formationClass, setFormationClass] = useState<string>(`formation-${formationCode}`);
 
   return (
-    <PlayerSelectOptionsContext.Provider value={players}>
-      <div className={formationClass}>
-        <FootballField style={style}>
-          {positionCodes.map((position) => {
-            const playerRef = useRef<HTMLDivElement>(null);
+    <div className={formationClass}>
+      <FootballField style={style}>
+        {positionCodes.map((position) => {
+          const playerRef = useRef<HTMLDivElement>(null);
 
-            return (
-              <Fragment key={position}>
-                <PlayerIcon
-                  position={position}
-                  player={defaultPlayer({ number: position })}
-                  readonly={readonly}
-                  ref={playerRef}
-                />
-              </Fragment>
-            );
-          })}
-          <FormationIcon formationCode={formationCode} onChange={onChange} readonly={readonly} />
-        </FootballField>
-      </div>
-    </PlayerSelectOptionsContext.Provider>
+          return (
+            <Fragment key={position}>
+              <PlayerIcon
+                position={position}
+                player={defaultPlayer({ number: position })}
+                readonly={readonly}
+                ref={playerRef}
+              />
+            </Fragment>
+          );
+        })}
+        <FormationIcon formationCode={formationCode} onChange={onChange} readonly={readonly} />
+      </FootballField>
+    </div>
   );
 };
 
