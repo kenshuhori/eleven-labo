0e3437ea915339fa1a1f8e2056040d5da432f77b
Kenshu Hori <kenshuhori@gmail.com>
2024-08-08 23:15:52 +0900

    feat: ユーザー名を表示

--- src/utils/authorAvatar.ts
+++ src/utils/authorAvatar.ts
@@ -1,7 +1,7 @@
 import type { User } from "@prisma/client";
 
 export const authorAvatar = (author: User): { name: string; image: string } => {
-  const authorName = author.firstName ?? author.username ?? "Unknown";
+  const authorName = `${author.lastName ?? ""}${author.firstName ?? ""}` ?? "Unknown";
   const authorImage = author.imageUrl;
 
   return {
